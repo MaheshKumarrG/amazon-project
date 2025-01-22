7d94e775ff294d9ace759106316115f257dd2015
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-01-22 21:21:21 +0530

    using promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,9 +3,19 @@ import {renderPayment} from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 // import '../data/cart-class.js'
 // import '../data/backend.js'
-loadProducts(()=>{
+
+new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve();
+    })
+}).then(()=>{
     renderPayment();
     renderCheckout();
 });
 
+/*
+loadProducts(()=>{
+    
+});
+*/
 
