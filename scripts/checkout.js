a71d521d11789c1c38e940218e00ba0fadf229d9
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-01-22 13:45:39 +0530

    practicing backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import {renderCheckout} from "./checkout/orderSummary.js";
 import {renderPayment} from "./checkout/paymentSummary.js";
 // import '../data/cart-class.js'
+import '../data/backend.js'
 
 renderCheckout();
 renderPayment();
